97a6cbe626e7c2e2f26b93c5a9f736adfe9c8b68
Udit Sharma <uditsharma8959@gmail.com>
2025-07-27 07:12:39 +0530

    Added modular prediction response files

--- ML model FastAPI/app.py
+++ ML model FastAPI/app.py
@@ -1,47 +1,46 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-from pydantic import BaseModel, Field, computed_field, field_validator
 from schema.user_input import UserInput
-from typing import Literal, Annotated
-import pickle
-import pandas as pd
-
-# import the ml model
-with open('model/model.pkl', 'rb') as f:
-    model = pickle.load(f)
-
-MODEL_VERSION = "Version 1"
+from schema.prediction_response import PredictionResponse
+from Model.predict import predict_output, model, MODEL_VERSION
 
 app = FastAPI()
-        
+
+# human readable       
 @app.get('/')
 def home():
-    return {"message" : "Premium Prediction Website"}
+    return {'message':'Insurance Premium Prediction API'}
 
-# Creating this end point as this is needed when you use Kubernetes/ AWS 
-@app.get("/health")
+# machine readable
+@app.get('/health')
 def health_check():
     return {
-        'status' : 'OK',
-        'version' : MODEL_VERSION,
-        'model_loaded' : model is not None
+        'status': 'OK',
+        'version': MODEL_VERSION,
+        'model_loaded': model is not None
     }
 
-@app.post('/predict')
+@app.post('/predict', response_model=PredictionResponse)
 def predict_premium(data: UserInput):
 
-    input_df = pd.DataFrame([{
+    user_input = {
         'bmi': data.bmi,
         'age_group': data.age_group,
         'lifestyle_risk': data.lifestyle_risk,
         'city_tier': data.city_tier,
         'income_lpa': data.income_lpa,
         'occupation': data.occupation
-    }])
+    }
 
-    prediction = model.predict(input_df)[0]
+    try:
 
-    return JSONResponse(status_code=200, content={'predicted_category': prediction})
+        prediction = predict_output(user_input)
+
+        return JSONResponse(status_code=200, content={'response': prediction})
+    
+    except Exception as e:
+
+        return JSONResponse(status_code=500, content=str(e))
 
 
 
