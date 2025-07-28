006fd84eca2fd9419df9a3ce8afc3424b23de051
Udit Sharma <uditsharma8959@gmail.com>
2025-07-28 18:14:48 +0530

    Added all the files

--- ML model FastAPI/Model/predict.py
+++ ML model FastAPI/Model/predict.py
@@ -2,7 +2,7 @@ import pickle
 import pandas as pd
 
 # import the ml model
-with open('model/model.pkl', 'rb') as f:
+with open('Model/model.pkl', 'rb') as f:
     model = pickle.load(f)
 
 # MLFlow
